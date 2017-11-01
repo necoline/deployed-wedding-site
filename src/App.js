214338a68e6144eaca300a63aa5e18f9c210219c
necoline <hnhubner@gmail.com>
2017-10-31 22:18:37 -0700

    adding decorative line

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Rsvp from './Rsvp.js';
+import DecorationLine from './DecorationLine.js';
 
 class App extends Component {
   render() {
@@ -10,8 +11,13 @@ class App extends Component {
           <h1 className="text title blue">Patrick + Necoline </h1>
         </header>
         <div className="main-body">
-          {/* <img src="../public/decLine.svg" /> */}
-          <p className="title text blue body-title">Please RSVP by January 15, 2018</p>
+          <div className="header">
+            <DecorationLine />
+            <p className="title text blue body-title">
+              Please RSVP by January 15, 2018
+            </p>
+            <DecorationLine />
+          </div>
           <Rsvp />
         </div>
       </div>
